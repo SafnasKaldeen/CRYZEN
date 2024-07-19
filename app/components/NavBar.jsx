d4d472e2158fee583bf3fe369f9d410482e14931
SafnasKaldeen <110978617+SafnasKaldeen@users.noreply.github.com>
2024-07-20 04:10:07 +0530

    search and nav

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -4,36 +4,47 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FaSearch } from "react-icons/fa";
-import ThemeToggler from "./ThemeToggler";
+import OrderBy from "../components/OrderBy";
 
 const NavBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <div className="flex justify-between items-center py-6 px-10 text-white">
-      <Link href="/">
-        <Image
-          src="/logo.png"
-          width={70}
-          height={70}
-          alt="logo"
-          className="transition-transform transform hover:scale-110"
-        />
-      </Link>
-      <form className="relative w-full mx-20" action={`/search/${searchQuery}`}>
-        <input
-          type="text"
-          placeholder="Search 870474 games"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="w-full py-2 pl-10 pr-4 bg-gray-700/30 text-white placeholder-gray-400 border border-gray-600 rounded-full focus:border-gray-500 focus:outline-none transition-all duration-300 focus:bg-black/40 hover:border-blue-400/40"
-        />
-        <Link href={`/search/${searchQuery}`}>
-          <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer" />
+    <React.Fragment>
+      <div className="flex justify-between items-center py-6 px-10 text-white">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            width={70}
+            height={70}
+            alt="logo"
+            className="transition-transform transform hover:scale-110"
+          />
         </Link>
-      </form>
-      {/* <ThemeToggler /> */}
-    </div>
+        <div className="relative w-full mx-8">
+          <input
+            type="text"
+            placeholder="Search 870474 games"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full py-2 pl-10 pr-4 bg-gray-700/30 text-white placeholder-gray-400 border border-gray-600 rounded-full focus:border-gray-500 focus:outline-none transition-all duration-300 focus:bg-black/40 hover:border-blue-400/40"
+          />
+          <Link href={`/search/${searchQuery}`}>
+            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer" />
+          </Link>
+        </div>
+
+        {/* `OrderBy` should be hidden on smaller screens */}
+        <div className="hidden sm:block">
+          <OrderBy />
+        </div>
+      </div>
+
+      {/* `OrderBy` centered and taking full width on smaller screens */}
+      <div className="sm:hidden flex justify-center w-full py-4">
+        <OrderBy />
+      </div>
+    </React.Fragment>
   );
 };
 
---
+++ app/components/OrderBy.jsx
@@ -0,0 +1,100 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+
+const OrderBy = () => {
+  const [selectedValue, setSelectedValue] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc"); // default sort order
+  const [shouldDisplay, setShouldDisplay] = useState(true);
+
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setSelectedValue(newValue);
+
+    // Update the URL with the selected order parameter and sort order
+    const currentUrl = new URL(window.location.href);
+    currentUrl.searchParams.set(
+      "ordering",
+      sortOrder === "asc" ? newValue : `-${newValue}`
+    );
+    window.history.pushState(null, "", currentUrl.toString());
+  };
+
+  const toggleSortOrder = () => {
+    const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+    setSortOrder(newSortOrder);
+
+    // Update the URL with the selected order parameter and new sort order
+    const currentUrl = new URL(window.location.href);
+    currentUrl.searchParams.set(
+      "ordering",
+      newSortOrder === "asc" ? selectedValue : `-${selectedValue}`
+    );
+    window.history.pushState(null, "", currentUrl.toString());
+  };
+
+  // Initialize the selected value and sort order based on the current URL query parameter
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const orderParam = urlParams.get("ordering");
+    const path = window.location.pathname;
+
+    if (path.includes("/games/")) {
+      setShouldDisplay(false);
+    } else {
+      if (orderParam) {
+        const isDescending = orderParam.startsWith("-");
+        const value = isDescending ? orderParam.substring(1) : orderParam;
+        setSelectedValue(value);
+        setSortOrder(isDescending ? "desc" : "asc");
+      }
+    }
+  }, []);
+
+  if (!shouldDisplay) return null; // Don't render anything if the component should not be displayed
+
+  return (
+    <div className="form-control w-full max-w-xs py-0 px-2">
+      <div className="flex items-center space-x-2">
+        <p
+          className={`text-xs ${
+            sortOrder === "asc" ? "text-green-400" : "text-red-400"
+          }`}
+        >
+          {sortOrder === "asc" ? "Ascending" : "Descending"}
+        </p>
+        <button
+          className="flex items-center justify-center p-2 text-gray-400"
+          onClick={toggleSortOrder}
+        >
+          {sortOrder === "asc" ? <FaChevronUp /> : <FaChevronDown />}
+        </button>
+        <select
+          id="order-select"
+          className="select select-bordered text-base py-2 pl-10 pr-4 bg-gray-700/30 text-white placeholder-zinc-900 border border-gray-600 rounded-full focus:border-gray-500 focus:outline-none transition-all duration-300 focus:bg-black/40 hover:border-blue-400/40"
+          value={selectedValue}
+          onChange={handleChange}
+        >
+          <option value="" disabled>
+            {selectedValue ? `Order By: ${selectedValue}` : "Select an option"}
+          </option>
+          <option className="bg-zinc-900" value="released">
+            Released
+          </option>
+          <option className="bg-zinc-900" value="name">
+            Name
+          </option>
+          <option className="bg-zinc-900" value="rating">
+            Rating
+          </option>
+          <option className="bg-zinc-900" value="metacritic">
+            Metacritic
+          </option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default OrderBy;
